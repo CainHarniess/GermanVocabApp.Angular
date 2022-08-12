13b722d2d15ec3121b75a9fb3cac6d1de493a085
CainHarniess <cainharniess@gmail.com>
2022-08-12 23:07:55 +0100

    Renamed word type form folder

--- src/app/vocab-forms/vocab-forms.module.ts
+++ src/app/vocab-forms/vocab-forms.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { NounFormComponent } from './word-type-form/noun-form/noun-form.component';
+import { NounFormComponent } from './word-type-forms/noun-form/noun-form.component';
 import { VocabAppFormsModule } from '../forms/vocab-app-forms.module';
 import { VocabListFormComponent } from './vocab-list-form/vocab-list-form.component';
 import { VocabListItemFormComponent } from './vocab-list-item-form/vocab-list-item-form.component';
 import { SharedModule } from '../shared/shared.module';
-import { VerbFormComponent } from './word-type-form/verb-form/verb-form.component';
-import { AdjectiveFormComponent } from './word-type-form/adjective-form/adjective-form.component';
-import { AdverbFormComponent } from './word-type-form/adverb-form/adverb-form.component';
+import { VerbFormComponent } from './word-type-forms/verb-form/verb-form.component';
+import { AdjectiveFormComponent } from './word-type-forms/adjective-form/adjective-form.component';
+import { AdverbFormComponent } from './word-type-forms/adverb-form/adverb-form.component';
 
 @NgModule({
   declarations: [
