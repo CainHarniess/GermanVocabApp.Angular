8435d7292f73be37348690af7a15f0de576cf1d7
CainHarniess <cainharniess@gmail.com>
2022-08-20 13:35:40 +0100

    Moved the word type drop down options into separate data class.

---
+++ src/app/vocab-forms/vocab-list-item-form/drop-down-options.class.ts
@@ -0,0 +1,18 @@
+import { SingleSelectOption } from "../../forms/single-select/single-select-option.interface";
+import { WordType } from "../../vocab/models/data/word-type.enum";
+
+export class DropDownOptions {
+  public static readonly wordTypeOptions: SingleSelectOption<WordType>[] = [{
+    value: WordType.Noun,
+    label: "Noun",
+  }, {
+    value: WordType.Verb,
+    label: "Verb",
+  }, {
+    value: WordType.Adjective,
+    label: "Adjective",
+  }, {
+    value: WordType.Adverb,
+    label: "Adverb",
+  }];
+}
--- src/app/vocab-forms/vocab-list-item-form/vocab-list-item-form.component.ts
+++ src/app/vocab-forms/vocab-list-item-form/vocab-list-item-form.component.ts
@@ -2,9 +2,9 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output
 import { FormArray, FormGroup } from '@angular/forms';
 
 import { map, Observable } from 'rxjs';
-import { SingleSelectOption } from '../../forms/single-select/single-select-option.interface';
 
 import { WordType } from '../../vocab/models/data/word-type.enum';
+import { DropDownOptions } from './drop-down-options.class';
 
 @Component({
   selector: 'app-vocab-list-item-form',
@@ -14,19 +14,7 @@ import { WordType } from '../../vocab/models/data/word-type.enum';
 })
 export class VocabListItemFormComponent implements OnInit {
   public readonly WordType: typeof WordType = WordType;
-  public readonly wordTypeOptions: SingleSelectOption<WordType>[] = [{
-    value: WordType.Noun,
-    label: "Noun",
-  }, {
-    value: WordType.Verb,
-    label: "Verb",
-  }, {
-    value: WordType.Adjective,
-    label: "Adjective",
-  }, {
-    value: WordType.Adverb,
-    label: "Adverb",
-  }];
+  public readonly dropDownOptions: typeof DropDownOptions = DropDownOptions;
 
   public wordType$!: Observable<WordType>;
 
