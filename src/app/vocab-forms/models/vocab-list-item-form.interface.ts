880fe9784fbf991d86a5b1a9d3a30a9cadbbcfc5
CainHarniess <cainharniess@gmail.com>
2022-08-22 22:00:49 +0100

    Set all controls to be not optional in the vocab list item form model.

--- src/app/vocab-forms/models/vocab-list-item-form.interface.ts
+++ src/app/vocab-forms/models/vocab-list-item-form.interface.ts
@@ -7,22 +7,22 @@ import { WordType } from "../../vocab/models/data/word-type.enum";
 
 export interface VocabListItemForm {
   wordType: FormControl<WordType | null>;
-  isWeakMasculineNoun?: FormControl<boolean | null>;
-  reflexiveCase?: FormControl<ReflexiveCase | null>;
-  isSeparable?: FormControl<boolean | null>;
-  isTransitive?: FormControl<boolean | null>;
-  thirdPersonPresent?: FormControl<string | null>;
-  thirdPersonImperfect?: FormControl<string | null>;
-  auxiliaryVerb?: FormControl<AuxiliaryVerb | null>;
-  perfect?: FormControl<string | null>;
-  gender?: FormControl<Gender | null>;
+  isWeakMasculineNoun: FormControl<boolean | null>;
+  reflexiveCase: FormControl<ReflexiveCase | null>;
+  isSeparable: FormControl<boolean | null>;
+  isTransitive: FormControl<boolean | null>;
+  thirdPersonPresent: FormControl<string | null>;
+  thirdPersonImperfect: FormControl<string | null>;
+  auxiliaryVerb: FormControl<AuxiliaryVerb | null>;
+  perfect: FormControl<string | null>;
+  gender: FormControl<Gender | null>;
   german: FormControl<string | null>;
-  plural?: FormControl<string | null>;
-  preposition?: FormControl<string | null>;
-  prepositionCase?: FormControl<Case | null>;
-  comparative?: FormControl<string | null>;
-  superlative?: FormControl<string | null>;
+  plural: FormControl<string | null>;
+  preposition: FormControl<string | null>;
+  prepositionCase: FormControl<Case | null>;
+  comparative: FormControl<string | null>;
+  superlative: FormControl<string | null>;
   english: FormControl<string | null>;
-  fixedPlurality?: FormControl<FixedPlurality | null>
-  isIrregular?: FormControl<boolean | null>;
+  fixedPlurality: FormControl<FixedPlurality | null>
+  isIrregular: FormControl<boolean | null>;
 }
