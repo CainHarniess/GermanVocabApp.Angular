afd1f61d38e457c285572e9e9b2df4effc85f71f
CainHarniess <cainharniess@gmail.com>
2022-10-25 21:33:53 +0100

    Added vocab list and item validation provider tests.

--- src/app/vocab-forms/services/tests/item-validation.provider.spec.ts
+++ src/app/vocab-forms/services/tests/item-validation.provider.spec.ts
@@ -1,9 +1,14 @@
 import { ValidatorFn, Validators } from "@angular/forms";
+import { wordMaxLength, wordMinLength } from "../../../vocab/models/data/constraints/item-data-constraints";
 import { ItemValidationProvider } from "../item-validation.provider";
 
 describe(ItemValidationProvider.name, () => {
   let provider: ItemValidationProvider;
-  let validatorFactory: any;
+  let validatorFactory: any = {
+    create: {}
+  };
+  let factoryCreateSpy: any;
+  // TODO: Refactor declaration shared with list validation provider into a shared builder or something.
   let form: any = {
     controls: {
       wordType: {
@@ -23,11 +28,16 @@ describe(ItemValidationProvider.name, () => {
   let factoryValidator: (minLength: number) => ValidatorFn = Validators.minLength;
 
   beforeEach(() => {
-    validatorFactory = {
-      create: { }
-    };
-    spyOn(validatorFactory, "create").and.returnValue(factoryValidator);
+
+    factoryCreateSpy = spyOn(validatorFactory, "create").and.returnValue(factoryValidator);
     provider = new ItemValidationProvider(validatorFactory);
+  });
+
+  describe("constructor", () => {
+    it("Build the validators with the correct values", () => {
+      expect(factoryCreateSpy).toHaveBeenCalledTimes(2);
+      expect(factoryCreateSpy).toHaveBeenCalledWith(wordMinLength, wordMaxLength);
+    });
   });
 
   describe("addValidationTo", () => {
@@ -37,13 +47,13 @@ describe(ItemValidationProvider.name, () => {
       expect(form.controls.wordType.addValidators).toHaveBeenCalledOnceWith([Validators.required]);
     });
 
-    it("Should add required validator to word type control", () => {
+    it("Should add factory validator to english control", () => {
       spyOn(form.controls.english, "addValidators")
       provider.addValidationTo(form);
       expect(form.controls.english.addValidators).toHaveBeenCalledOnceWith([factoryValidator]);
     });
 
-    it("Should add required validator to word type control", () => {
+    it("Should add factory validator to german control", () => {
       spyOn(form.controls.german, "addValidators")
       provider.addValidationTo(form);
       expect(form.controls.german.addValidators).toHaveBeenCalledOnceWith([factoryValidator]);
