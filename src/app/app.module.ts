2eb0244f594c6f9618ea539a98fd9195ba72c05b
CainHarniess <cainharniess@gmail.com>
2022-11-01 12:33:41 +0000

    Added simple error handling to the http vocab list service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
-import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotificationService } from '../core';
+import { ConsoleLogger, ConsoleLogWriter, Logger, LogService, Severity, SeverityStringConverter } from '../core/logging';
+import { MatSnackBarService } from './angular-material';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
-import { VocabListService } from './vocab/services/vocab-list.service';
-import { HttpVocabListService } from './vocab/services/http-vocab-list.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InMemoryVocabListService } from './vocab/services/in-memory-vocab-list.service';
-import { InMemoryDataProvider } from './vocab/services/in-memory-data-seeder.service';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
-import { HttpErrorInterceptor } from '../core/error-handling';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { NotificationService } from '../core';
-import { MatSnackBarService } from './angular-material';
-import { ConsoleLogger, ConsoleLogWriter, Logger, LogService, Severity, SeverityStringConverter } from '../core/logging';
-import { GlobalErrorHandler } from '../core/error-handling/global-error-handler';
+import { HttpVocabListService } from './vocab/services/http-vocab-list.service';
+import { VocabListService } from './vocab/services/vocab-list.service';
 
 @NgModule({
   declarations: [
@@ -43,11 +39,9 @@ import { GlobalErrorHandler } from '../core/error-handling/global-error-handler'
     { provide: Logger, useClass: ConsoleLogger },
     LogService,
     { provide: VocabListService, useClass: HttpVocabListService },
-    { provide: ErrorHandler, useClass: GlobalErrorHandler},
     ////InMemoryDataProvider,
     //{ provide: VocabListService, useClass: InMemoryVocabListService },
     { provide: "minLevel", useValue: Severity.Information },
-    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
     { provide: NotificationService, useClass: MatSnackBarService },
     MatSnackBar,
   ],
--- src/app/vocab/services/http-vocab-list.service.ts
+++ src/app/vocab/services/http-vocab-list.service.ts
@@ -1,25 +1,44 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, throwError } from 'rxjs';
+import { NotificationService } from '../../../core';
 
 import { VocabList, VocabListItem } from '../models';
 import { VocabListService } from './vocab-list.service';
 
+// TODO: Provide this in the vocab module only.
+// TODO: Rename this (and base) to "VocabService".
 @Injectable({ providedIn: "root" })
 export class HttpVocabListService extends VocabListService {
 
-  constructor(private readonly http: HttpClient) {
+  constructor(private readonly http: HttpClient,
+    private readonly notificationService: NotificationService) {
     super();
   }
 
   private readonly url: string = "/api/vocab-lists";
 
   public override get(): Observable<VocabList[]> {
-    return this.http.get<VocabList[]>(this.url);
+    return this.http.get<VocabList[]>(this.url)
+      .pipe(
+        catchError((e: any, caught: Observable<VocabList[]>) => {
+          const message: string = "Unable to retrieve vocab lists.";
+
+          if (e instanceof HttpErrorResponse) {
+            const notification: string = `${message}\r\n${e.status} ${e.statusText}\r\n${e.message}`;
+            this.notificationService.error(notification);
+            return throwError(() => new Error(notification));
+          }
+
+          this.notificationService.error(message);
+          const error: Error = new Error(message)
+          return throwError(() => error);
+        }),
+      );
   }
 
   public override getWithId(vocabListId: string): Observable<VocabList> {
-    return this.http.get<VocabList>(this.url + `/${ vocabListId }`);
+    return this.http.get<VocabList>(this.url + `/${vocabListId}`);
   }
 
   public override add(vocabList: VocabList): Observable<string> {
